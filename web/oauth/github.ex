53c2a251a22bc73aa96c2a9d25d0ae6529d1d499
Maor Leger <maor.leger@gmail.com>
2017-05-06 17:16:32 -0600

    github oauth strategy

---
+++ web/oauth/github.ex
@@ -0,0 +1,38 @@
+defmodule GitHub do
+  use OAuth2.Strategy
+
+  alias OAuth2.Strategy.AuthCode
+
+  def client do
+    OAuth2.Client.new([
+      strategy: __MODULE__,
+      client_id: System.get_env("GITHUB_CLIENT_ID"),
+      client_secret: System.get_env("GITHUB_CLIENT_SECRET"),
+      redirect_uri: System.get_env("GITHUB_REDIRECT_URI"),
+      site: "https://api.github.com",
+      authorize_url: "https://github.com/login/oauth/authorize",
+      token_url: "https://github.com/login/oauth/access_token"
+    ])
+  end
+
+  def authorize_url!(params \\ []) do
+    OAuth2.Client.authorize_url!(client(), params)
+  end
+
+  def get_token!(params \\ [], headers \\ []) do
+    OAuth2.Client.get_token!(client(), Keyword.merge(params, client_secret: client().client_secret))
+  end
+
+  # Strategy Callbacks
+  
+  def authorize_url(client, params) do
+    AuthCode.authorize_url(client, params)
+  end
+
+  def get_token(client, params, headers) do
+    client
+    |> put_header("Accept", "application/json")
+    |> AuthCode.get_token(params, headers)
+  end
+  
+end
