13a1cf298a6caae397ea57281ef5814a659f3b66
Maor Leger <maor.leger@gmail.com>
2017-05-17 12:36:45 -0600

    [#145395621] move over user repo test

---
+++ test/models/user_repo_test.exs
@@ -0,0 +1,25 @@
+defmodule Tracker2x2.UserRepoTest do
+  use Tracker2x2.ModelCase
+  alias Tracker2x2.User
+  alias Tracker2x2.Repo
+
+  @valid_attrs %{email: "maor.leger@example.com", tracker_token: "some token", encryption_version: "some version"}
+
+  def changeset(email) do
+    attrs = Map.put(@valid_attrs, :email, email)
+    User.changeset(%User{}, attrs)
+  end
+
+  test "errors when unique constraint fails" do
+    error = {:email, {"has already been taken", []}}
+    changeset("maor") |> Repo.insert!()
+    assert {:error, new_changeset} = Repo.insert(changeset("maor"))
+    assert new_changeset.errors == [error]
+  end
+
+  test "email is required" do
+    error = {:email, {"can't be blank", [validation: :required]}}
+    assert {:error, new_changeset} = Repo.insert(changeset(nil))
+    assert new_changeset.errors == [error]
+  end
+end
--- test/plugs/auth_test.exs
+++ test/plugs/auth_test.exs
@@ -3,4 +3,17 @@ defmodule Tracker2x2.AuthTest do
   import Plug.Test
   alias Tracker2x2.Auth
 
+  setup %{conn: conn} do
+    conn =
+      conn
+      |> bypass_through(Tracker2x2.Router, :browser)
+      |> get("/")
+
+    {:ok, %{conn: conn}}
+  end
+
+  test "authenticate_user halts when no current_user exists",  %{conn: conn} do
+
+  end
+
 end
