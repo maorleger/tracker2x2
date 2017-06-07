201c91089365da0b45eb13aaae851cc576db5131
Maor Leger <maor.leger@gmail.com>
2017-06-07 09:00:37 -0600

    [#145808543] implement the behaviour callback

--- lib/tracker2x2/tracker_api.ex
+++ lib/tracker2x2/tracker_api.ex
@@ -5,4 +5,7 @@ defmodule Tracker2x2.TrackerApi do
 
   # credo:disable-for-next-line Credo.Check.Readability.MaxLineLength
   @callback get_epics(project_id :: String.t, tracker_token :: String.t) :: {atom, %{status_code: integer, response_body: %{}}}
+
+  # credo:disable-for-next-line Credo.Check.Readability.MaxLineLength
+  @callback get_stories(project_id :: String.t, epic :: String.t, tracker_token :: String.t) :: {atom, %{}}
 end
